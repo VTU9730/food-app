3c05a337d5114c4078a1ce08bfe03b844591308c
VTU9730 <prathapsiddavatam@gmail.com>
2024-07-20 09:29:45 +0530

    routes-added

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
 import Main from './src/Components/Main';
+import About from './src/Components/About';
+import Error from './src/Components/Error';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Main />)
+const appRoter = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main />,
+        errorElement: <Error />
+    },
+    {
+        path:'/about',
+        element: <About />
+    }
+])
+
+root.render(<RouterProvider router={appRoter}/>)
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,5 @@
 const Card = (props) => {
-    const {cloudinaryImageId, avgRatingString, locality, name, cuisines, id} = props.restaurant.info
-    console.log("child");
+    const {cloudinaryImageId, avgRatingString, locality, name, cuisines, id} = props.restaurant.info;
     return(
         <div className="card-container">
             <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
