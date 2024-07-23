f89a84fc810421d5fbbef892dfc5c3e109a1045b
VTU9730 <prathapsiddavatam@gmail.com>
2024-07-23 22:25:59 +0530

    tailwindcss added

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -34,17 +34,20 @@ const Body = () => {
   
   return (
     <div>
-      <div className="search-container">
+      <div className="flex justify-center">
         <div>
-          <input type="search" onChange={(e) => setSearchText(e.target.value)} value = {searchText} />
-          <button onClick={()=>{
+          <input className="border-orange-600 w-200 h-8" type="search" onChange={(e) => setSearchText(e.target.value)} value = {searchText} />
+          <button
+            className=" bg-green-200"
+            onClick={()=>{
             const filteredRestaurantData = restaurantData.filter(res => res.info.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
             setFilteredRestaurant(filteredRestaurantData);
             console.log(filteredRestaurantData)
           }}>Search</button>
         </div>
-        <div>
+        <div className="align-middle">
           <button
+            className=" bg-orange-200 mx-4"
             onClick={() => {
               const filteredRestaurantData = restaurantData.filter((res) => res.info.avgRating > 4.3);
               setFilteredRestaurant(filteredRestaurantData);
@@ -56,7 +59,7 @@ const Body = () => {
         </div>
       </div>
       
-      <div className="cards-container">
+      <div className="flex flex-wrap justify-center">
         {filteredRestaurant.map((restaurant) => (
           <div key={restaurant.info.id}>
             {" "}
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,9 +3,9 @@ const Card = (props) => {
     
     console.log("hello");
     return(
-        <div className="card-container">
-            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
-            <h1>{name}</h1>
+        <div className="w-52 h-auto mx-4 m-8 bg-slate-200 hover:bg-slate-400">
+            <img className="w-52 h-52 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
+            <h1 className="font-bold">{name}</h1>
             <h2>{avgRatingString} Stars</h2>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{locality}</h4>
