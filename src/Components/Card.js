c0d5e70d7ba3e5c4c2c268090c8acaeed2540c91
VTU9730 <prathapsiddavatam@gmail.com>
2024-07-25 22:09:19 +0530

    improved styles

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,8 @@
 const Card = (props) => {
+    console.log(props);
     const {cloudinaryImageId, avgRatingString, locality, name, cuisines, id} = props.restaurant.info;
-    
-    console.log("hello");
     return(
-        <div className="w-52 h-auto mx-4 m-8 bg-slate-200 hover:bg-slate-400">
+        <div className="w-52 p-2 h-full bg-slate-200 hover:bg-slate-400">
             <img className="w-52 h-52 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
             <h1 className="font-bold">{name}</h1>
             <h2>{avgRatingString} Stars</h2>
